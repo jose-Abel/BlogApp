a1bf66c1891a3e5d08ccb9ebb40fb6fc9957d071
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-14 12:13:10 -0400

    Add semantic-ui-react to the client-app

--- client-app/src/app/layouts/App.tsx
+++ client-app/src/app/layouts/App.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import './App.css';
 import { Article } from "../models/article";
 import axios from 'axios';
+import { Button, Container, Header, Menu, Segment } from 'semantic-ui-react';
 
 function App() {
   const [articles, setArticles] = useState<Article[] | undefined>([]);
@@ -15,13 +16,70 @@ function App() {
   }, []);
 
   return (
-    <div>
-      {articles?.map((article) => {
-        return (
-          <div key={article.id}>{article.title}</div>
-        )
-      })}
-    </div>
+    <Fragment>
+      <Segment
+        inverted
+        textAlign='center'
+        style={{ minHeight: 700, padding: '1em 0em' }}
+        vertical
+      >
+        <Menu
+          fixed= 'top'
+          inverted
+          pointing
+          secondary
+          size='large'
+        >
+          <Container>
+            <Menu.Item as='a' active>
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>Work</Menu.Item>
+            <Menu.Item as='a'>Company</Menu.Item>
+            <Menu.Item as='a'>Careers</Menu.Item>
+            <Menu.Item position='right'>
+              <Button as='a' inverted>
+                Log in
+              </Button>
+              <Button as='a' inverted primary style={{ marginLeft: '0.5em' }}>
+                Sign Up
+              </Button>
+            </Menu.Item>
+          </Container>
+        </Menu>
+        <Container text>
+          <Header
+            as='h1'
+            content='Imagine-a-Company'
+            inverted
+            style={{
+              fontSize: '4em',
+              fontWeight: 'normal',
+              marginBottom: 0,
+              marginTop: '3em',
+            }}
+          />
+          <Header
+            as='h2'
+            content='Do whatever you want when you want to.'
+            inverted
+            style={{
+              fontSize: '1.7em',
+              fontWeight: 'normal',
+              marginTop: '1.5em',
+            }}
+          />
+        </Container>
+      </Segment>
+      
+      <div>
+        {articles?.map((article) => {
+          return (
+            <div key={article.id}>{article.title}</div>
+          )
+        })}
+      </div>
+    </Fragment>
   );
 }
 
